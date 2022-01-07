bea57217da21be52a563d34eb35ff11dbaaa4641
tamirshina <tamirshina@gmail.com>
2022-01-07 11:29:16 +0200

    local file

---
+++ local.js
@@ -0,0 +1,14 @@
+
+//git push heroku main
+const localVars = {
+
+    //get token
+    client_id_token: '0ea34b53-f759-4559-93d3-25481847b392',
+    client_secret_token: 'XA6w-E~mHpXduNL7_W_-f080V_aa7uYJ9x',
+
+    //create lead
+    client_id_lead: 'f9ba45d3480840c18ab01c3f46d151ec',
+    client_secret_lead: '4e6f2A5d30C2480fB14F130ff9a89793'
+}
+
+module.exports.localVars = localVars;
