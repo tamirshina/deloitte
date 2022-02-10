9d8dda90fc2f1ec986239172e451785182dd49b7
tamirshina <tamirshina@gmail.com>
2022-02-10 11:06:15 +0200

    moving to prod

--- api/data.js
+++ api/data.js
@@ -1,14 +1,14 @@
 const apiVariables = {
 
     getTokenVars: {
-        resource: 'https://internal-crm-api-sandbox.deloitteresources.com/uat/v1/marketing',
+        resource: 'https://internal-crm-api.deloitteresources.com/v1/marketing',
         url: 'https://login.microsoftonline.com:443/36da45f1-dd2c-4d1f-af13-5abe46b99921/oauth2/token'
     },
     createLeadVars: {
-        host: 'crm-api-sandbox.deloitteresources.com',
+        host: 'crm-api.deloitteresources.com',
         pathToKey: '../../keys/ssl-certificate/ssl-certificate',
         pathToLocalKey: '../../keys/ssl-certificate/ssl-certificate',
-        pathToLead: '/uat/v1/marketing/leadBulk'
+        pathToLead: '/v1/marketing/leadBulk'
     }
 
 
